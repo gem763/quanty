14cd525d9040dbd49578ecb00eacec5de1b99998
gem763 <gem763@hanmail.net>
2018-05-25 18:12:37 +0900

    dual_momentum.py 속도개선중

--- model/dual_momentum.py
+++ model/dual_momentum.py
@@ -7,8 +7,8 @@ from numba import njit, float64, int64, int32, boolean
 
 @njit(float64[:](int64, int64[:], float64[:,:], int32[:,:]))
 def _signal_nb(i_date, i_ref, p_ref_val, sig_w):
-    r = p_ref_val[i_date] / p_ref_val[i_ref[i_ref<i_date][-12:]] - 1.0
-    r *= sig_w
+    r = p_ref_val[i_date] / p_ref_val[i_ref[i_ref<i_date][-len(sig_w):]] - 1.0
+    r *= sig_w[-r.shape[0]:]
     
     n = p_ref_val.shape[1]
     out = np.empty(n)
@@ -22,8 +22,8 @@ def _signal_nb(i_date, i_ref, p_ref_val, sig_w):
 @njit(boolean(int64, int64, int64, float64[:,:], int64))
 def _has_ma_mtum_single_nb(i_date, term_short, term_long, p_ref_val, i_asset):
     p = p_ref_val[:i_date+1,i_asset]
-    p_ma_short = p[-term_short:].mean()
-    p_ma_long = p[-term_long:].mean()
+    p_ma_short = np.nanmean(p[-term_short:])
+    p_ma_long = np.nanmean(p[-term_long:])
     return p_ma_short>p_ma_long
     
 
@@ -65,7 +65,7 @@ class DualMomentum(object):
     def get(self, date):
         i_date = self.p_ref.index.get_loc(date, method='ffill')
         sig_ = self._signal(i_date)
-        selection_, ranks_ = self._selection(sig_, i_date, date)
+        selection_, ranks_ = self._selection(sig_, i_date)
         return selection_, ranks_, sig_
     
     
@@ -79,35 +79,31 @@ class DualMomentum(object):
         n_back = n_sig_w*31 + 40
         date_from = date - n_back*Day()
         date_to = date - 0*Day()
-        #set_trace()
+
         p = self.p_ref.loc[date_from:date_to].resample('M').ffill().iloc[-n_sig_w-1:]
-        #p = self.p_ref.loc[date_from:date_to].resample('M').asfreq().iloc[-n_sig_w-1:]
         r = (p.iloc[-1]/p.iloc[:-1]-1).replace(np.inf, np.nan)
-        sig_w = self.sig_w[-len(r):]
-        
+        sig_w = self.sig_w[-len(r):].reshape(1,-1)[0]
         sig = r.mul(sig_w, axis=0).sum(skipna=False)        
         sig.index = self.assets_member.bet
         
-        not_tradable = ~self._is_tradable(date)
-        sig.loc[not_tradable] = np.nan
+        #not_tradable = ~self._is_tradable(date)
+        #sig.loc[not_tradable] = np.nan
         return sig
     
     
     def _is_tradable(self, i_date):
-        #return self.p_close.loc[:date].iloc[-1].notnull()
         return self.p_close.iloc[i_date].notnull()
             
     
-    def _selection(self, sig, i_date, date):
+    def _selection(self, sig, i_date):
         is_tradable = self._is_tradable(i_date)
         sig.loc[~is_tradable] = np.nan
         
-        #is_rf_tradable = self._is_tradable(date, self.riskfree)
-        has_rf_ma_mtum = self._has_ma_mtum_single(date, self.rf_trend, self.riskfree)
+        has_rf_ma_mtum = self._has_ma_mtum_single(i_date, self.rf_trend, self.riskfree)
         has_rf_positive_sig = sig.loc[self.riskfree]>=0
         
         if self.support_cash and is_tradable[self.riskfree] and (has_rf_ma_mtum or has_rf_positive_sig):
-            pos, ranks = self._get_default_selection(date, sig, self.n_picks-1, i_date)
+            pos, ranks = self._get_default_selection(i_date, sig, self.n_picks-1)
             pos_rf = self.n_picks - pos.sum()
             pos_cash = 0
             
@@ -121,7 +117,7 @@ class DualMomentum(object):
                 pos_rf = int(pos_rf*0.5)
           
         else:
-            pos, ranks = self._get_default_selection(date, sig, self.n_picks, i_date)
+            pos, ranks = self._get_default_selection(i_date, sig, self.n_picks)
             pos_rf = 0
 
             if is_tradable[self.cash_equiv]:
@@ -142,11 +138,8 @@ class DualMomentum(object):
         return pos, ranks
     
     
-    def _get_default_selection(self, date, sig, n_picks, i_date):
-        has_ma_mtum = self._has_ma_mtum(i_date, self.self_trend)
-        
-        score = sig.copy()
-        score.loc[~has_ma_mtum] = np.nan
+    def _get_default_selection(self, i_date, sig, n_picks):
+        score = self._screen_by_ma_mtum(sig.copy(), i_date, self.self_trend)
         ranks = score.rank(ascending=False, na_option='bottom')
         
         if self.mode=='DualMomentum':
@@ -165,31 +158,16 @@ class DualMomentum(object):
         return pos.astype(int), ranks
     
     
-    
-    def _has_ma_mtum(self, i_date, terms):
+    def _screen_by_ma_mtum(self, score, i_date, terms):
         if terms is not None:
-            #p = self.p_ref.loc[:date]
-            #p_ma_short = p.iloc[-terms[0]:].mean()
-            #p_ma_long = p.iloc[-terms[1]:].mean()
-            #has_ma_mtum = p_ma_short > p_ma_long
-            #set_trace()
             has_ma_mtum = _has_ma_mtum_nb(i_date, terms[0], terms[1], self.p_ref_val)
-            #has_ma_mtum = pd.Series(has_ma_mtum, index=self.assets_member.bet)
-            #has_ma_mtum.index = self.assets_member.bet
-        
-        else:
-            has_ma_mtum = pd.Series(index=self.assets_member.bet)
-            has_ma_mtum[:] = True
+            score.loc[~has_ma_mtum] = np.nan
             
-        return has_ma_mtum
-
+        return score
+        
       
     def _has_ma_mtum_single(self, i_date, terms, asset_ref):
         if terms is not None:
-            #p = self.p_ref.loc[:date, asset_ref]
-            #p_ma_short = p.iloc[-terms[0]:].mean()
-            #p_ma_long = p.iloc[-terms[1]:].mean()
-            #has_ma_mtum = p_ma_short > p_ma_long
             i_asset = self.p_ref.columns.get_loc(asset_ref)
             has_ma_mtum = _has_ma_mtum_single_nb(i_date, terms[0], terms[1], self.p_ref_val, i_asset)
         
@@ -198,3 +176,15 @@ class DualMomentum(object):
             
         return has_ma_mtum
     
+    
+    def _has_ma_mtum_single2(self, date, terms, asset_ref):
+        if terms is not None:
+            p = self.p_ref.loc[:date, asset_ref]
+            p_ma_short = p.iloc[-terms[0]:].mean()
+            p_ma_long = p.iloc[-terms[1]:].mean()
+            has_ma_mtum = p_ma_short > p_ma_long
+        
+        else:
+            has_ma_mtum = True
+            
+        return has_ma_mtum
